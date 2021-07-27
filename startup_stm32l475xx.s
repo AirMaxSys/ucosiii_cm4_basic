bee4dff273a5ffcafdb175b2aeb701cef85a3c98
AirMaxSys <gy520813@live.com>
2021-07-27 17:38:17 +0800

    fix vector table - pendSVC and sysTick

--- startup_stm32l475xx.s
+++ startup_stm32l475xx.s
@@ -143,8 +143,8 @@ g_pfnVectors:
 	.word	SVC_Handler
 	.word	DebugMon_Handler
 	.word	0
-	.word	PendSV_Handler
-	.word	SysTick_Handler
+	.word   OS_CPU_PendSVHandler
+	.word   OS_CPU_SysTickHandler
 	.word	WWDG_IRQHandler
 	.word	PVD_PVM_IRQHandler
 	.word	TAMP_STAMP_IRQHandler
