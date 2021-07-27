155b6df7b1cbc78f3be28936757698fc00f6845e
AirMaxSys <gy520813@live.com>
2021-07-27 17:33:30 +0800

    update OS kernel version v3.08.01

--- RTOS/uC-OS3/Ports/ARM-Cortex-M/ARMv7-M/os_cpu_c.c
+++ RTOS/uC-OS3/Ports/ARM-Cortex-M/ARMv7-M/os_cpu_c.c
@@ -1,46 +1,34 @@
 /*
 *********************************************************************************************************
-*                                                uC/OS-III
-*                                          The Real-Time Kernel
+*                                              uC/OS-III
+*                                        The Real-Time Kernel
 *
+*                    Copyright 2009-2021 Silicon Laboratories Inc. www.silabs.com
 *
-*                           (c) Copyright 2009-2017; Micrium, Inc.; Weston, FL
-*                    All rights reserved.  Protected by international copyright laws.
+*                                 SPDX-License-Identifier: APACHE-2.0
 *
-*                                              ARMv7-M Port
+*               This software is subject to an open source license and is distributed by
+*                Silicon Laboratories Inc. pursuant to the terms of the Apache License,
+*                    Version 2.0 available at www.apache.org/licenses/LICENSE-2.0.
 *
-* File      : OS_CPU_C.C
-* Version   : V3.06.02
-* By        : JJL
-*             BAN
-*             JBL
+*********************************************************************************************************
+*/
+
+/*
+*********************************************************************************************************
 *
-* LICENSING TERMS:
-* ---------------
-*           uC/OS-III is provided in source form for FREE short-term evaluation, for educational use or
-*           for peaceful research.  If you plan or intend to use uC/OS-III in a commercial application/
-*           product then, you need to contact Micrium to properly license uC/OS-III for its use in your
-*           application/product.   We provide ALL the source code for your convenience and to help you
-*           experience uC/OS-III.  The fact that the source is provided does NOT mean that you can use
-*           it commercially without paying a licensing fee.
+*                                             ARMv7-M Port
 *
-*           Knowledge of the source code may NOT be used to develop a similar product.
-*
-*           Please help us continue to provide the embedded community with the finest software available.
-*           Your honesty is greatly appreciated.
-*
-*           You can find our product's user manual, API reference, release notes and
-*           more information at doc.micrium.com.
-*           You can contact us at www.micrium.com, or by phone at +1 (954) 217-2036.
-*
-* For       : ARMv7M Cortex-M
-* Mode      : Thumb-2 ISA
-* Toolchain : GNU C Compiler
-*
-* Note(s)   : (1) This port supports the ARM Cortex-M3, Cortex-M4 and Cortex-M7 architectures.
-*             (2) It has been tested with the following Hardware Floating Point Unit.
-*                 (a) Single-precision: FPv4-SP-D16-M and FPv5-SP-D16-M
-*                 (b) Double-precision: FPv5-D16-M
+* File    : os_cpu_c.c
+* Version : V3.08.01
+*********************************************************************************************************
+* For     : ARMv7-M Cortex-M
+* Mode    : Thumb-2 ISA
+*********************************************************************************************************
+* Note(s) : (1) This port supports the ARM Cortex-M3, Cortex-M4 and Cortex-M7 architectures.
+*           (2) It has been tested with the following Hardware Floating Point Unit.
+*               (a) Single-precision: FPv4-SP-D16-M and FPv5-SP-D16-M
+*               (b) Double-precision: FPv5-D16-M
 *********************************************************************************************************
 */
 
@@ -57,12 +45,20 @@ const  CPU_CHAR  *os_cpu_c__c = "$Id: $";
 *********************************************************************************************************
 */
 
-#include  "../../../../Source/os.h"
+#include  "../../../Source/os.h"
 
 
 #ifdef __cplusplus
 extern  "C" {
 #endif
+
+/*
+*********************************************************************************************************
+*                                       LOCAL GLOBAL VARIABLES
+*********************************************************************************************************
+*/
+
+CPU_INT32U  OS_KA_BASEPRI_Boundary;                             /* Base Priority boundary.                              */
 
 
 /*
@@ -132,6 +128,8 @@ void  OSInitHook (void)
         }
     }
 #endif
+                                                                /* Set BASEPRI boundary from the configuration.         */
+    OS_KA_BASEPRI_Boundary = (CPU_INT32U)(CPU_CFG_KA_IPL_BOUNDARY << (8u - CPU_CFG_NVIC_PRIO_BITS));
 }
 
 
@@ -146,7 +144,7 @@ void  OSInitHook (void)
 * Note(s)    : None.
 *********************************************************************************************************
 */
-#if (OS_CFG_TASK_STK_REDZONE_EN == DEF_ENABLED)
+#if (OS_CFG_TASK_STK_REDZONE_EN > 0u)
 void  OSRedzoneHitHook (OS_TCB  *p_tcb)
 {
 #if OS_CFG_APP_HOOKS_EN > 0u
@@ -453,27 +451,6 @@ CPU_STK  *OSTaskStkInit (OS_TASK_PTR    p_task,
                                                                 /* Align the stack to 8-bytes.                          */
     p_stk = (CPU_STK *)((CPU_STK)(p_stk) & 0xFFFFFFF8u);
                                                                 /* Registers stacked as if auto-saved on exception      */
-#if (OS_CPU_ARM_FP_EN > 0u)                                     /* FPU auto-saved registers.                            */
-     --p_stk;
-    *(--p_stk) = (CPU_STK)0x02000000u;                          /* FPSCR                                                */
-                                                                /* Initialize S0-S15 floating point registers           */
-    *(--p_stk) = (CPU_STK)0x41700000u;                          /* S15                                                  */
-    *(--p_stk) = (CPU_STK)0x41600000u;                          /* S14                                                  */
-    *(--p_stk) = (CPU_STK)0x41500000u;                          /* S13                                                  */
-    *(--p_stk) = (CPU_STK)0x41400000u;                          /* S12                                                  */
-    *(--p_stk) = (CPU_STK)0x41300000u;                          /* S11                                                  */
-    *(--p_stk) = (CPU_STK)0x41200000u;                          /* S10                                                  */
-    *(--p_stk) = (CPU_STK)0x41100000u;                          /* S9                                                   */
-    *(--p_stk) = (CPU_STK)0x41000000u;                          /* S8                                                   */
-    *(--p_stk) = (CPU_STK)0x40E00000u;                          /* S7                                                   */
-    *(--p_stk) = (CPU_STK)0x40C00000u;                          /* S6                                                   */
-    *(--p_stk) = (CPU_STK)0x40A00000u;                          /* S5                                                   */
-    *(--p_stk) = (CPU_STK)0x40800000u;                          /* S4                                                   */
-    *(--p_stk) = (CPU_STK)0x40400000u;                          /* S3                                                   */
-    *(--p_stk) = (CPU_STK)0x40000000u;                          /* S2                                                   */
-    *(--p_stk) = (CPU_STK)0x3F800000u;                          /* S1                                                   */
-    *(--p_stk) = (CPU_STK)0x00000000u;                          /* S0                                                   */
-#endif
     *(--p_stk) = (CPU_STK)0x01000000u;                          /* xPSR                                                 */
     *(--p_stk) = (CPU_STK)p_task;                               /* Entry Point                                          */
     *(--p_stk) = (CPU_STK)OS_TaskReturn;                        /* R14 (LR)                                             */
@@ -482,12 +459,7 @@ CPU_STK  *OSTaskStkInit (OS_TASK_PTR    p_task,
     *(--p_stk) = (CPU_STK)0x02020202u;                          /* R2                                                   */
     *(--p_stk) = (CPU_STK)p_stk_limit;                          /* R1                                                   */
     *(--p_stk) = (CPU_STK)p_arg;                                /* R0 : argument                                        */
-
-#if (OS_CPU_ARM_FP_EN > 0u)
-    *(--p_stk) = (CPU_STK)0xFFFFFFEDuL;                         /* R14: EXEC_RETURN; See Note 5                         */
-#else
     *(--p_stk) = (CPU_STK)0xFFFFFFFDuL;                         /* R14: EXEC_RETURN; See Note 5                         */
-#endif
                                                                 /* Remaining registers saved on process stack           */
     *(--p_stk) = (CPU_STK)0x11111111uL;                         /* R11                                                  */
     *(--p_stk) = (CPU_STK)0x10101010uL;                         /* R10                                                  */
@@ -497,26 +469,6 @@ CPU_STK  *OSTaskStkInit (OS_TASK_PTR    p_task,
     *(--p_stk) = (CPU_STK)0x06060606uL;                         /* R6                                                   */
     *(--p_stk) = (CPU_STK)0x05050505uL;                         /* R5                                                   */
     *(--p_stk) = (CPU_STK)0x04040404uL;                         /* R4                                                   */
-
-#if (OS_CPU_ARM_FP_EN > 0u)
-                                                                /* Initialize S16-S31 floating point registers          */
-    *(--p_stk) = (CPU_STK)0x41F80000u;                          /* S31                                                  */
-    *(--p_stk) = (CPU_STK)0x41F00000u;                          /* S30                                                  */
-    *(--p_stk) = (CPU_STK)0x41E80000u;                          /* S29                                                  */
-    *(--p_stk) = (CPU_STK)0x41E00000u;                          /* S28                                                  */
-    *(--p_stk) = (CPU_STK)0x41D80000u;                          /* S27                                                  */
-    *(--p_stk) = (CPU_STK)0x41D00000u;                          /* S26                                                  */
-    *(--p_stk) = (CPU_STK)0x41C80000u;                          /* S25                                                  */
-    *(--p_stk) = (CPU_STK)0x41C00000u;                          /* S24                                                  */
-    *(--p_stk) = (CPU_STK)0x41B80000u;                          /* S23                                                  */
-    *(--p_stk) = (CPU_STK)0x41B00000u;                          /* S22                                                  */
-    *(--p_stk) = (CPU_STK)0x41A80000u;                          /* S21                                                  */
-    *(--p_stk) = (CPU_STK)0x41A00000u;                          /* S20                                                  */
-    *(--p_stk) = (CPU_STK)0x41980000u;                          /* S19                                                  */
-    *(--p_stk) = (CPU_STK)0x41900000u;                          /* S18                                                  */
-    *(--p_stk) = (CPU_STK)0x41880000u;                          /* S17                                                  */
-    *(--p_stk) = (CPU_STK)0x41800000u;                          /* S16                                                  */
-#endif
 
     return (p_stk);
 }
@@ -546,12 +498,8 @@ void  OSTaskSwHook (void)
 #ifdef  CPU_CFG_INT_DIS_MEAS_EN
     CPU_TS  int_dis_time;
 #endif
-#if (OS_CFG_TASK_STK_REDZONE_EN == DEF_ENABLED)
+#if (OS_CFG_TASK_STK_REDZONE_EN > 0u)
     CPU_BOOLEAN  stk_status;
-#endif
-
-#if (OS_CPU_ARM_FP_EN > 0u)
-    OS_CPU_FP_Reg_Push(OSTCBCurPtr->StkPtr);                    /* Push the FP registers of the current task.           */
 #endif
 
 #if OS_CFG_APP_HOOKS_EN > 0u
@@ -587,16 +535,12 @@ void  OSTaskSwHook (void)
     OSSchedLockTimeMaxCur = (CPU_TS)0;                          /* Reset the per-task value                             */
 #endif
 
-#if (OS_CFG_TASK_STK_REDZONE_EN == DEF_ENABLED)
+#if (OS_CFG_TASK_STK_REDZONE_EN > 0u)
                                                                 /* Check if stack overflowed.                           */
-    stk_status = OSTaskStkRedzoneChk(DEF_NULL);
-    if (stk_status != DEF_OK) {
+    stk_status = OSTaskStkRedzoneChk((OS_TCB *)0u);
+    if (stk_status != OS_TRUE) {
         OSRedzoneHitHook(OSTCBCurPtr);
     }
-#endif
-
-#if (OS_CPU_ARM_FP_EN > 0u)
-    OS_CPU_FP_Reg_Pop(OSTCBHighRdyPtr->StkPtr);                 /* Pop the FP registers of the highest ready task.      */
 #endif
 }
 
@@ -667,12 +611,16 @@ void  OS_CPU_SysTickHandler  (void)
 
 void  OS_CPU_SysTickInitFreq (CPU_INT32U  cpu_freq)
 {
+#if (OS_CFG_TICK_EN > 0u)
     CPU_INT32U  cnts;
 
 
     cnts = (cpu_freq / (CPU_INT32U)OSCfg_TickRate_Hz);          /* Determine nbr SysTick cnts between two OS tick intr. */
 
     OS_CPU_SysTickInit(cnts);
+#else
+    (void)cpu_freq;
+#endif
 }
 
 
@@ -692,23 +640,30 @@ void  OS_CPU_SysTickInitFreq (CPU_INT32U  cpu_freq)
 
 void  OS_CPU_SysTickInit (CPU_INT32U  cnts)
 {
+#if (OS_CFG_TICK_EN > 0u)
     CPU_INT32U  prio;
+    CPU_INT32U  basepri;
 
 
-    CPU_REG_NVIC_ST_RELOAD = cnts - 1u;
+                                                                /* Set BASEPRI boundary from the configuration.         */
+    basepri             = (CPU_INT32U)(CPU_CFG_KA_IPL_BOUNDARY << (8u - CPU_CFG_NVIC_PRIO_BITS));
+    CPU_REG_SYST_RVR    = cnts - 1u;                            /* Set Reload Register                                  */
 
-                                                            /* Set SysTick handler prio.                              */
-    prio                   = CPU_REG_NVIC_SHPRI3;
-    prio                  &= DEF_BIT_FIELD(24, 0);
-    prio                  |= DEF_BIT_MASK(OS_CPU_CFG_SYSTICK_PRIO, 24);
+                                                                /* Set SysTick handler prio.                            */
+    prio                = CPU_REG_SCB_SHPRI3;
+    prio               &= 0x00FFFFFFu;
+    prio               |= (basepri << 24u);
 
-    CPU_REG_NVIC_SHPRI3    = prio;
+    CPU_REG_SCB_SHPRI3  = prio;
 
-                                                            /* Enable timer.                                          */
-    CPU_REG_NVIC_ST_CTRL  |= CPU_REG_NVIC_ST_CTRL_CLKSOURCE |
-                             CPU_REG_NVIC_ST_CTRL_ENABLE;
-                                                            /* Enable timer interrupt.                                */
-    CPU_REG_NVIC_ST_CTRL  |= CPU_REG_NVIC_ST_CTRL_TICKINT;
+                                                                /* Enable timer.                                        */
+    CPU_REG_SYST_CSR   |= CPU_REG_SYST_CSR_CLKSOURCE |
+                          CPU_REG_SYST_CSR_ENABLE;
+
+    CPU_REG_SYST_CSR   |= CPU_REG_SYST_CSR_TICKINT;             /* Enable timer interrupt.                              */
+#else
+    (void)cnts;
+#endif
 }
 
 #ifdef __cplusplus
